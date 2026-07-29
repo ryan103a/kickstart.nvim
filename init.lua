3ea7f67a159103229ac34ce13256823075af1276
ryan103a <ryan103a@gmail.com>
2026-07-29 11:19:33 +1000

    changed to rosepine

--- init.lua
+++ init.lua
@@ -385,9 +385,9 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
+  vim.pack.add { gh 'rose-pine/neovim' }
   ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
+  require('rose-pine').setup {
     styles = {
       comments = { italic = false }, -- Disable italics in comments
     },
@@ -396,7 +396,7 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'rose-pine'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
